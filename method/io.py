66f97275d0a3cd2bedf92df1bd7a5b3503a788bf
Chon Lok Lei <chonloklei@gmail.com>
2021-01-20 18:51:03 +0800

    Add rama data for TP53 and update io for loading new data type

--- method/io.py
+++ method/io.py
@@ -40,3 +40,39 @@ def load_training_density(filedir):
         densities.append(np.reshape(pp, d_shape))
         labels.append([[[0, 1]]])
     return np.asarray(densities), np.asarray(labels)
+
+
+def load_training_rama(filedir):
+    """
+    # Load the MD rama data of the classification.
+    #
+    # Input
+    # =====
+    # `filedir`: (str) The directory containing the data.
+    #
+    # Return
+    # =====
+    # `densities`: (list) A list of 2-D arrays of the densities.
+    # `labels`: (list) A list of arrays of the labels arranged as
+    #           (is_begnin, is_pathogenic).
+    """
+    import os
+    import glob
+    d_shape = (334, 217, 2)  # rama shape: (time_frame, protein_size, phi_psi)
+    bp = os.path.join(filedir, 'Benign/rama_csv')
+    pp = os.path.join(filedir, 'Pathogenic/rama_csv')
+    bs = glob.glob(bp + '/*_rama.csv')
+    ps = glob.glob(pp + '/*_rama.csv')
+
+    # Load
+    densities = []
+    labels = []
+    for b in bs:
+        bb = np.loadtxt(b, delimiter=',', usecols=[0, 1])  # skip last col.
+        densities.append(np.reshape(bb, d_shape).reshape(-1, 217 * 2))
+        labels.append([[[1, 0]]])
+    for p in ps:
+        pp = np.loadtxt(p, delimiter=',', usecols=[0, 1])  # skip last col.
+        densities.append(np.reshape(pp, d_shape).reshape(-1, 217 * 2))
+        labels.append([[[0, 1]]])
+    return np.asarray(densities), np.asarray(labels)
