6fc0033183cda0eeb3bdd245db7c6d02ea2d88c3
Chon Lok Lei <chonloklei@gmail.com>
2021-01-21 15:55:49 +0800

    Update pca-knn to try abeta data from McCoy et al. 2021

--- pca-knn.py
+++ pca-knn.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python3
 import method.io as io
 import numpy as np
+import argparse
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
 
-# Load data
-x, l = io.load_training_rama('data/TP53')
+parser = argparse.ArgumentParser('Training PCA-KNN classifier')
+parser.add_argument('--seed', type=int, default=0,
+                    help='Seeding of the run')
+parser.add_argument('-d', '--data', type=str, choices=['tp53', 'abeta'],
+                    default='abeta', help='Data for testing the method')
+args = parser.parse_args()
 
-# Split data
-x_train, x_test, l_train, l_test = train_test_split(
-    x, l, test_size=0.2, random_state=1, shuffle=True
-)
+if args.data == 'tp53':
+    # Load data
+    x, l = io.load_training_rama('data/TP53')
 
-# Reshape data
-x_train = x_train.reshape(-1, 217 * 2)
-x_test = x_test.reshape(-1, 217 * 2)
+    # Split data
+    x_train, x_test, l_train, l_test = train_test_split(
+        x, l, test_size=0.2, random_state=args.seed, shuffle=True
+    )
+
+    # Reshape data
+    x_train = x_train.reshape(-1, 217 * 2)
+    x_test = x_test.reshape(-1, 217 * 2)
+elif args.data == 'abeta':
+    import pandas
+    abeta = pandas.read_csv('data/abeta2.csv')
+    l = np.zeros(abeta.shape[0])
+    l[abeta.phenotype1 == 'Pathogenic'] = 1
+    x = np.asarray(np.asarray(abeta)[:, 3:], dtype=float)
+    test = (abeta.grouping == 'E22D') | (abeta.grouping == 'A21G')
+    train = ~test
+    x_train = x[train]
+    y_train = l[train]
+    x_test = x[test]
+    y_test = l[test]
 
 # Transform data
 scaler = StandardScaler()
@@ -26,7 +47,7 @@ x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
 # Set seed
-np.random.seed(1)
+np.random.seed(args.seed)
 
 # PCA
 pca = PCA()
@@ -35,15 +56,16 @@ x_train = pca.transform(x_train)
 x_test = pca.transform(x_test)
 
 # Redo labels
-y_train = []
-for l in l_train:
-    y_train += [l[0, 0, 1]] * 334  # 334 frame per variant
-y_test = []
-for l in l_test:
-    y_test += [l[0, 0, 1]] * 334
+if args.data == 'tp53':
+    y_train = []
+    for l in l_train:
+        y_train += [l[0, 0, 1]] * 334  # 334 frame per variant
+    y_test = []
+    for l in l_test:
+        y_test += [l[0, 0, 1]] * 334
 
 # Set seed
-np.random.seed(1)
+np.random.seed(args.seed)
 
 # KNN
 knn = KNeighborsClassifier(n_neighbors=1)
@@ -52,10 +74,13 @@ y_pred = knn.predict(x_test[:, :100])
 print(confusion_matrix(y_test, y_pred))
 
 # Compute centroid
-x_train = x_train.reshape(-1, 334, 217 * 2)
-x_test = x_test.reshape(-1, 334, 217 * 2)
-x_train = np.mean(x_train, axis=1)
-x_test = np.mean(x_test, axis=1)
+if args.data == 'tp53':
+    x_train = x_train.reshape(-1, 334, 217 * 2)
+    x_test = x_test.reshape(-1, 334, 217 * 2)
+    x_train = np.mean(x_train, axis=1)
+    x_test = np.mean(x_test, axis=1)
+elif args.data == 'abeta':
+    raise NotImplementedError
 
 # KNN
 knn = KNeighborsClassifier(n_neighbors=1)
